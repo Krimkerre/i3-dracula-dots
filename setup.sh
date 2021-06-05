b82906a487ce3a5ae0b2b097b4c753a6fac64ff3
Krimkerre <66228690+Krimkerre@users.noreply.github.com>
2021-06-05 18:43:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -118,7 +118,7 @@ echo "##########################################################################
 # System tools
 yay -S autotiling   --noconfirm --needed
 yay -S qt5-styleplugins   --noconfirm --needed
-yay -S pfetch-git   --noconfirm --needed
+yay -S ufetch-git   --noconfirm --needed
 
 
 # Internet
